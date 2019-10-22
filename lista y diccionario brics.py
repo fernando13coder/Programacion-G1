8dc4883c0cbe9cd16b382d91a34c4eb0883c6865
fernando13coder <56598707+fernando13coder@users.noreply.github.com>
2019-10-21 21:04:01 -0500

    Add files via upload

---
+++ lista y diccionario brics.py
@@ -0,0 +1,8 @@
+countries=["Brazil","Russia","India","China","South Africa"]
+capitals={"Brazil":"Brasilia","Russia":"Moscow","India":"New Delhi","China":"Beijing","South Africa":["Pretoria","Cape Town","Bloemfontein"]}
+
+print(countries)
+print(capitals)
+print(capitals["South Africa"][1])
+print(capitals["South Africa"][0])
+print(capitals["South Africa"][2])
